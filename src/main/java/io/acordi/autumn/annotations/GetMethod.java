66d1ec30b1b596e032395912ca5171404d12e66b
Pedro Acordi <pedroacordi2@gmail.com>
2025-02-25 14:12:58 -0300

    feat: add annotations

---
+++ src/main/java/io/acordi/autumn/annotations/GetMethod.java
@@ -0,0 +1,15 @@
+package io.acordi.autumn.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+
+public @interface GetMethod {
+
+    public String value() default "";
+
+}
